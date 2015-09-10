2503650a76e4be7a8845644d6e30bddaf6ef5cf1
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2015-09-10 13:17:05 -0300

    Change in structure, for better documentation and use.

--- src/component_presence.rs
+++ src/component_presence.rs
@@ -10,6 +10,34 @@ use std::ops::{Deref, DerefMut};
 /// but that only should be used when you are certain that component is present in the entity.
 /// 
 /// You can check presence with `has_it` or `lacks_it`
+///
+/// # Examples
+///
+/// ```
+/// let mut manager = EntityManager::new();
+/// let ent = manager.new_entity();
+/// manager.add_component_to(ent, Position{x:1, y:2});
+/// // Makes use of `Deref` to get a reference of `Position` out of `ComponentPresence`
+/// println!("pos: {:?}", manager.data[ent].position.x);
+/// // Makes use of `DerefMut` to get a mutable reference of `Position`
+/// manager.data[ent].position.x += 5;
+/// ```
+///
+/// # Panics
+/// Trying to access a component that isn't held by the entity will panic.
+/// 
+/// ```
+/// let mut manager = EntityManager::new();
+/// let ent = manager.new_entity();
+/// println!("pos: {:?}", manager.data[ent].position.x);
+/// ```
+///
+/// Always be sure that the entity does pocess the component.
+/// 
+/// If you are only accessing components inside a `System` then that is already handled
+/// for the systems required components.
+///
+/// Otherwise use `has_it` or a `match`.
 #[derive(Debug)]
 pub enum ComponentPresence<T> {
     Comp(T),
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ use std::collections::HashMap;
 use std::ops::{Index, IndexMut};
 
 pub mod component_presence;
-use component_presence::ComponentPresence;
-use component_presence::ComponentPresence::*;
 
 /// Type Entity is simply an ID used as indexes.
 pub type Entity = u32;
@@ -20,36 +18,10 @@ macro_rules! components {
         $([$access:ident, $ty:ty]),+
             ) => {
 
-        /// ComponentData 
-        pub struct ComponentData {
-            components: HashMap<Entity, EntityData>,
-        }
-
-        /// This trait marks a struct as a component.
-        ///
-        /// It should implement the `add_to` function, which is automatically generated
-        /// by the `components` macro.
-        pub trait Component {
-            fn add_to(self, ent: Entity, data: &mut ComponentData);
-        }
-
-        impl ComponentData {
-            pub fn new() -> ComponentData {
-                ComponentData {
-                    components: HashMap::new(),
-                }
-            }
-
-            pub fn get(&self, ent: &Entity) -> &EntityData {
-                self.components.get(ent).expect("no entity")
-            }
-
-            pub fn get_mut(&mut self, ent: &Entity) -> &mut EntityData {
-                self.components.get_mut(ent).expect("No entity")
-            }
-        }
-
-        /// EntityData
+        use $crate::component_presence::ComponentPresence;
+        use $crate::component_presence::ComponentPresence::*;
+        use $crate::{EntityDataHolder, Component, Entity, ComponentData};
+        
         pub struct EntityData {
             $(
                 pub $access: ComponentPresence<$ty>,
@@ -57,7 +29,7 @@ macro_rules! components {
         }
 
         impl EntityData {
-            pub fn new() -> EntityData {
+            pub fn new_empty() -> EntityData {
                 EntityData {
                     $(
                         $access: None,
@@ -66,9 +38,15 @@ macro_rules! components {
             }
         }
 
+        impl EntityDataHolder for EntityData {
+            fn new() -> Self {
+                EntityData::new_empty()
+            }
+        }
+
         $(
-            impl Component for $ty {
-                fn add_to(self, ent: Entity, data: &mut ComponentData) {
+            impl Component<EntityData> for $ty {
+                fn add_to(self, ent: Entity, data: &mut ComponentData<EntityData>) {
                     let ent_data = data.components.get_mut(&ent).expect("no entity");
                     ent_data.$access = Comp(self);
                 }
@@ -76,49 +54,124 @@ macro_rules! components {
             )+
 
             
-            impl Index<Entity> for ComponentData {
-                type Output = EntityData;
 
-                fn index(&self, index: Entity) -> &EntityData {
-                    &self.get(&index)
-                }
-            }
 
-        impl IndexMut<Entity> for ComponentData {
-            fn index_mut(&mut self, index: Entity) -> &mut EntityData {
-                self.get_mut(&index)
-            }
+
+    }
+}
+
+
+/// This is a marker trait to be used by the `components!` macro.
+///
+/// This trait is implemented by `EntityData` which is a struct generated
+/// by the `components!` macro.
+///
+/// `EntityData` will be of the form:
+/// 
+/// ```
+/// struct EntityData {
+///     component1: ComponentPresence<Component1>,
+///     component2: ComponentPresence<Component2>,
+///     //etc...
+/// }
+/// ```
+///
+/// So it will have one field per component defined in the call to `components!`
+/// You'll access these fields directly when indexing the `data` field of the `EntityManager`
+pub trait EntityDataHolder {
+    fn new() -> Self;
+}
+
+/// ComponentData 
+pub struct ComponentData<D: EntityDataHolder> {
+    pub components: HashMap<Entity, D>,
+}
+
+/// This trait marks a struct as a component. (Automatically handled by macro `components!`)
+///
+/// It should implement the `add_to` function, which is automatically generated
+/// by the `components!` macro.
+pub trait Component<D: EntityDataHolder> {
+
+    /// Adds self to the specified entity. Called by the `EntityManager`
+    fn add_to(self, ent: Entity, data: &mut ComponentData<D>);
+}
+
+impl<D: EntityDataHolder> ComponentData<D> {
+    pub fn new() -> ComponentData<D> {
+        ComponentData {
+            components: HashMap::new(),
         }
+    }
 
+    pub fn get(&self, ent: &Entity) -> &D {
+        self.components.get(ent).expect("no entity")
+    }
 
-        /// The `EntityManager` type manages all the entities.
-        ///
-        /// It is in charge of creating and destroying entities.
-        /// It also takes care of adding or removing components, through the `ComponentData` it contains.
-        pub struct EntityManager {
-            entities: Vec<Entity>,
-            data: ComponentData,
+    pub fn get_mut(&mut self, ent: &Entity) -> &mut D {
+        self.components.get_mut(ent).expect("No entity")
+    }
+
+    pub fn create_component_data_for(&mut self, ent: Entity) {
+        self.components.insert(ent, D::new());
+    }
+}
+
+impl<D: EntityDataHolder> Index<Entity> for ComponentData<D> {
+    type Output = D;
+
+    fn index(&self, index: Entity) -> &D {
+        &self.get(&index)
+    }
+}
+
+impl<D: EntityDataHolder> IndexMut<Entity> for ComponentData<D> {
+    fn index_mut(&mut self, index: Entity) -> &mut D {
+        self.get_mut(&index)
+    }
+}
+
+/// The `EntityManager` type manages all the entities.
+///
+/// It is in charge of creating and destroying entities.
+/// It also takes care of adding or removing components, through the `ComponentData` it contains.
+///
+/// # Examples
+///
+/// Creating a new manager, and adding some (predefined) components to a new entity.
+///
+/// ```
+/// let mut manager = EntityManager::new();
+/// let ent = manager.new_entity();
+/// manager.add_component_to(ent, Position{x: 1, y: 2});
+/// ```
+pub struct EntityManager<D: EntityDataHolder> {
+    entities: Vec<Entity>,
+    pub data: ComponentData<D>,
+}
+
+impl<D: EntityDataHolder> EntityManager<D> {
+
+    /// Creates a new EntityManager
+    pub fn new() -> EntityManager<D> {
+        EntityManager{
+            entities: vec!(),
+            data: ComponentData::new(),
         }
+    }
 
-        impl EntityManager {
-            pub fn new() -> EntityManager {
-                EntityManager{
-                    entities: vec!(),
-                    data: ComponentData::new(),
-                }
-            }
+    /// Creates a new entity, assigning it an unused ID, returning that ID for further use.
+    pub fn new_entity(&mut self) -> Entity {
+        let ent = self.entities.len() as Entity;
+        self.entities.push(ent);
 
-            pub fn new_entity(&mut self) -> Entity {
-                let ent = self.entities.len() as Entity;
-                self.entities.push(ent);
-                self.data.components.insert(ent, EntityData::new());
-                ent
-            }
+        self.data.create_component_data_for(ent);
+        ent
+    }
 
-            pub fn add_component_to<C: Component>(&mut self, e: Entity, c: C) {
-                c.add_to(e, &mut self.data);
-            }
-        }
+    /// Adds the specified component to the entity.
+    pub fn add_component_to<C: Component<D>>(&mut self, e: Entity, c: C) {
+        c.add_to(e, &mut self.data);
     }
 }
 
