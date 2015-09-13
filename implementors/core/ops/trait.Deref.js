41eeba8797e1af02fe238971b8a08abe8daf8cb7
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2015-09-13 20:46:54 -0300

    Add new doc files

--- implementors/core/ops/trait.Deref.js
+++ implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['lib'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='enum' href='lib/component_presence/enum.ComponentPresence.html' title='lib::component_presence::ComponentPresence'>ComponentPresence</a>&lt;T&gt;",];
+implementors['lib'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='enum' href='lib/component_presence/enum.ComponentPresence.html' title='lib::component_presence::ComponentPresence'>ComponentPresence</a>&lt;T&gt;",];implementors['composite'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='enum' href='composite/component_presence/enum.ComponentPresence.html' title='composite::component_presence::ComponentPresence'>ComponentPresence</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
